5e0a392eb8152e89456555c1ee061ff7a95e2d19
Victor Sahlin <victorsahlin@hotmail.com>
2024-10-17 10:45:35 +0200

    Ändring i feature

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("tgttytyy");
-            Console.WriteLine("Ändra i feature");
+            Console.WriteLine("Ändring i feature" + Guid.NewGuid());
         }
     }
 }
